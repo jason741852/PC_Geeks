1be851b2eabbe69532f337bf95bb9d94cb378a17
woodyc <woodyc@sfu.ca>
2017-07-15 19:07:52 -0700

    routing for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainPageComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+      path: 'main',
+      component: MainPageComponent
+    },
+    {
+      path: 'login',
+      component: LoginPageComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
