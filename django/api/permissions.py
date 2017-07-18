21bee728db1a964ff4be63a95edc21e4c15e0431
Jason <jason741852@gmail.com>
2017-07-18 11:00:22 -0700

    Add user auth and superuser(admin) login info in a text file

---
+++ django/api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+from .models import Post
+
+class IsOwner(BasePermission):
+        """Custom permission class to allow only post owners to edit them"""
+
+        def has_object_permission(self, request, view, obj):
+            """Return True if permission is granted to the post owner."""
+            if isinstance(obj, Post):
+                    return obj.owner == request.user
+            return obj.owner == request.user
--- django/api/views.py
+++ django/api/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import PostSerializer
 from .models import Post
+from .permissions import IsOwner
 # Create your views here.
 
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behaviour of our rest api"""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,IsOwner)
 
     def perform_create(self, serializer):
         """Save the post data wen creating a new post"""
-        serializer.save()
+        serializer.save(owner=self.request.user)
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles GET, PUT, PATCH and DELE TE requests."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsOwner)
