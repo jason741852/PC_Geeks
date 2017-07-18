0a747e8d68c9f55813c2b7ab9f4fc31452b0d7b5
Chang Yook Ahn <cyahn@sfu.ca>
2017-07-18 11:44:09 -0700

    fixed minor issue with pcparts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.pcpartsService.getLatestPCParts()
       .subscribe(res => {
-        this.mostRecentPCParts.push({ 'adult': 0, 'backdrop_path': "/puV2PFq4VQPItaygizgag8jrXa.jpg", 'genre_ids': 102, 'id': 324852, 'original_language': "en", 'original_title': "GTX 1060", 'overview': 'Perfect condition', 'popularity': 250.123363, 'poster_path':"https://images10.newegg.com/NeweggImage/ProductImage/14-487-267-S99.jpg", 'released_date': "2017-06-30", 'title':"Video card", 'video': false, 'vote_average': 6.2, 'vote_count': 543, '__photo__': Object});
+        this.mostRecentPCParts.push({'adult': 0, 'backdrop_path': "/puV2PFq4VQPItaygizgag8jrXa.jpg", 'genre_ids': 102, 'price': 225, 'category': "Graphic Card", 'id': 100, 'original_language': "en", 'original_title': "GTX 1060", 'overview': 'Perfect condition', 'popularity': 250.123363, 'poster_path':"https://images10.newegg.com/NeweggImage/ProductImage/14-487-267-S99.jpg", 'released_date': "2017-06-30", 'title':"Video card", 'video': false, 'vote_average': 6.2, 'vote_count': 543, '__photo__': Object});
         console.log(this.mostRecentPCParts);
       });
   }
--- src/app/components/pcpart/pcpart.component.ts
+++ src/app/components/pcpart/pcpart.component.ts
@@ -12,6 +12,7 @@ import { AuthenticationService } from '../../service/authentication.service'
   styleUrls: ['./pcpart.component.css']
 })
 export class PCPartComponent implements OnInit {
+  mostRecentPCParts: Array<any> = []
 
   pcpart: Object
   comments: Array<any>
@@ -26,9 +27,9 @@ export class PCPartComponent implements OnInit {
 
   ngOnInit() {
     this.router.params.subscribe(params => {
-      let id = params['id']
-      this.loadContent(id)
-    })
+      this.mostRecentPCParts.push({'adult': 0, 'backdrop_path': "/puV2PFq4VQPItaygizgag8jrXa.jpg", 'genre_ids': 102, 'category': "Graphic Card", 'price': 225, 'id': 100, 'original_language': "en", 'original_title': "GTX 1060", 'overview': 'Perfect condition', 'popularity': 250.123363, 'poster_path':"https://images10.newegg.com/NeweggImage/ProductImage/14-487-267-S99.jpg", 'released_date': "2017-06-30", 'title':"Video card", 'video': false, 'vote_average': 6.2, 'vote_count': 543, '__photo__': Object});
+      console.log(this.mostRecentPCParts);
+    });
   }
 
   addComment() {
