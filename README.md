2281d57a1826d68666a08d2475dd534c0221cd3c
Chris <chrislee0419@gmail.com>
2017-07-22 17:28:15 -0700

    updates README.md and build.sh script to handle refreshing angular and django servers

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 ### To run the web app:
 1. Use `vagrant up` to launch the VM.
-2. Use `vagrant ssh` to get into the VM, then `cd ~/angular/` and `ng serve` to run the Angular server.
-3. Access `localhost:4200` on your host machine.
-  1. To refresh the Django backend server, use `vagrant ssh` to get into the VM, then `sudo touch reload.ini` to restart the uWSGI server.
- 
+2. Access `localhost:4200` on your host machine.
+3. Refresh the server using the 'build.sh' script.
 
 
 #PLEASE LOOK AT THE BRANCHES and NETWORK, WE ARE WORKING ON SEPERATE BRANCHES AND CAN'T MERGE IT INTO ONE BRANCH YET
--- build.sh
+++ build.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-vagrant ssh --command 'cd /home/ubuntu/angular; ng build'
+refresh-both() {
+	echo "Refreshing Angular and Django files..."
+	vagrant ssh --command 'cd /home/ubuntu/angular; ng build; cd /home/ubuntu; sudo touch reload.ini;'
+}
+
+print-help() {
+	echo "Use '-a' to only refresh Angular or '-d' to only refresh Django."
+	echo "No arguments or '-ad' will refresh both."
+}
+
+if [ $# -eq 1 ]; then
+	if [ $1 = '-a' ]; then
+		echo "Refreshing Angular files..."
+		vagrant ssh --command 'cd /home/ubuntu/angular; ng build;'
+	elif [ $1 = '-d' ]; then
+		echo "Refreshing Django files..."
+		vagrant ssh --command 'cd /home/ubuntu; sudo touch reload.ini;'
+	elif [ $1 = '-ad' ] || [ $1 = '-da' ]; then
+		refresh-both
+	elif [ $1 = '-help' ] || [ $1 = '--help' ]; then
+		print-help
+	else
+		echo "Invalid arguments!"
+		print-help
+	fi
+elif [ $# -gt 1 ]; then
+	echo "Too many arguments!"
+	print-help
+else
+	refresh-both
+fi
