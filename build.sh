644a95b4d954c18428e55cc8162dcb4ff2e25958
Chris <cla235@sfu.ca>
2017-07-26 23:27:19 -0700

    comments out 'config.ssh.shell' line in Vagrantfile, otherwise build.sh does not work remove unnecessary 'final-project' folder

--- build.sh
+++ build.sh
@@ -2,21 +2,41 @@
 
 refresh-both() {
 	echo "Refreshing Angular and Django files..."
-	vagrant ssh --command 'cd /home/ubuntu/angular; ng build; cd /home/ubuntu; sudo touch reload.ini;'
+	vagrant ssh -c '
+cd /home/ubuntu/angular;
+ng build;
+cd /home/ubuntu/django;
+python3 manage.py makemigrations;
+python3 manage.py migrate;
+cd /home/ubuntu;
+sudo touch reload.ini;'
 }
 
 print-help() {
 	echo "Use '-a' to only refresh Angular or '-d' to only refresh Django."
+	echo "Use '--d-migrate' to perform a database migration on Django."
 	echo "No arguments or '-ad' will refresh both."
 }
 
 if [ $# -eq 1 ]; then
 	if [ $1 = '-a' ]; then
-		echo "Refreshing Angular files..."
-		vagrant ssh --command 'cd /home/ubuntu/angular; ng build;'
+		echo "Rebuilding Angular files..."
+		vagrant ssh -c '
+cd /home/ubuntu/angular;
+ng build;'
 	elif [ $1 = '-d' ]; then
-		echo "Refreshing Django files..."
-		vagrant ssh --command 'cd /home/ubuntu; sudo touch reload.ini;'
+		echo "Refreshing uWSGI (Django)..."
+		vagrant ssh -c '
+cd /home/ubuntu;
+sudo touch reload.ini;'
+	elif [ $1 = '--d-migrate' ]; then
+		echo "Migrating Django database and refreshing uWSGI..."
+		vagrant ssh -c '
+cd /home/ubuntu/django;
+python3 manage.py makemigrations;
+python3 manage.py migrate;
+cd /home/ubuntu;
+sudo touch reload.ini;'
 	elif [ $1 = '-ad' ] || [ $1 = '-da' ]; then
 		refresh-both
 	elif [ $1 = '-help' ] || [ $1 = '--help' ]; then
