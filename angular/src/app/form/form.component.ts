50315d123483b908d69df48ea016471da74057ac
woodyc <woodyc@sfu.ca>
2017-08-04 17:12:33 -0700

    adding form validation

--- angular/src/app/form/form.component.ts
+++ angular/src/app/form/form.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { Sale } from '../_models/sale';
 import { SaleService } from '../_services/sale.service';
+
 
 @Component({
   selector: 'app-form',
@@ -12,19 +15,26 @@ import { SaleService } from '../_services/sale.service';
 export class FormComponent implements OnInit {
   sales: Sale[];
   selectedSale: Sale;
-
+  saleForm: FormGroup;
   manufacturers = [' ', 'AMD', 'Asus', 'ATI',
 'BFG', 'Biostar', 'Club 3D', 'Corsair', 'Dell', 'Diamond', 'ECS', 'EVGA', 'Gainward',
 'GALAX', 'Galaxy', 'Gigabyte', 'HIS', 'HP', 'Inno3D', 'Jaton', 'KFA2', 'Lenovo', 'MSI',
-'NVIDIA', 'OcUK', 'Palit', 'PNY', 'PowerColor', 'Sapphire', 'Sparkle', 'VisionTek', 'XFX', 'Zogis', 'Zotac']
+'NVIDIA', 'OcUK', 'Palit', 'PNY', 'PowerColor', 'Sapphire', 'Sparkle', 'VisionTek', 'XFX', 'Zogis', 'Zotac'];
 
-  quality = [' ', 'Excellent', 'Very Good', 'Good', 'Average', 'Poor']
+  quality = [' ', 'Excellent', 'Very Good', 'Good', 'Average', 'Poor'];
 
-  category = [' ', 'CPU', 'CPU Cooler', 'Motherboard', 'Memory', 'Storage', 'Video Card', 'Power Supply', 'Case']
+  category = [' ', 'CPU', 'CPU Cooler', 'Motherboard', 'Memory', 'Storage', 'Video Card', 'Power Supply', 'Case'];
+
+  createRequest = { 
+      item: '',
+      category: this.category[0],
+      quality: this.quality[0],
+      manufacturer: this.manufacturers[0],
+      price: ''};
 
   constructor(
         private router: Router,
-        private saleService: SaleService) { }
+        private saleService: SaleService) { };
 
   getSales(): void {
     this.saleService.getSales().then(sales => this.sales = sales);
@@ -32,17 +42,28 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSales();
+    this.saleForm = new FormGroup({
+    'item': new FormControl(this.createRequest.item, [Validators.required]),
+    'category': new FormControl(this.createRequest.category),
+    'quality': new FormControl(this.createRequest.quality),
+    'manufacturer': new FormControl(this.createRequest.manufacturer),
+    'price': new FormControl(this.createRequest.price, [Validators.required, Validators.min(0),
+    Validators.pattern('0-9')])
+  });
   }
 
-  add(item: string,
-      category: string,
-      quality: string,
-      manufacturer: string,
-      price: number): void {
-    this.saleService.create(item, category, quality, manufacturer, price)
+  add(): void {
+    this.saleService.create(
+    this.saleForm.get('item').value, 
+    this.saleForm.get('category').value, 
+    this.saleForm.get('quality').value,
+    this.saleForm.get('manufacturer').value,
+    this.saleForm.get('price').value)
       .then(sale => {
         this.sales.push(sale);
         this.selectedSale = null;
       });
   }
+  get item() { return this.saleForm.get('item'); }
+  get price() { return this.saleForm.get('price'); } 
 }
