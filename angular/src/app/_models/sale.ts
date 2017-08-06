d4ae16d68ecd13f02579ca719489872c78721ec0
brandonn <brandonn@sfu.ca>
2017-08-06 01:42:40 -0700

    refactored

--- angular/src/app/_models/sale.ts
+++ angular/src/app/_models/sale.ts
@@ -14,20 +14,3 @@ export class Sale {
   date_created: string;
   date_modified: string;
 }
-
-// export interface ISale {
-//   id: number;
-//   body: string;
-//   item: string; // item name ie: Gtx 1080 etc
-//   category: string; // item type ie: Video Card
-//   quality: string;
-//   manufacturer: string;
-//   price: number;
-//   location: string;
-//   latitude: number;
-//   longitude: number;
-//   owner_id: number;
-//   buyer_id: number;
-//   date_created: string;
-//   date_modified: string;
-// }
