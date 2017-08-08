e91210844dee26a4ecd351ce3655ff55c5c1c2bb
Alin Paunescu <apaunesc@sfu.ca>
2017-08-08 00:50:13 -0700

    navigates to dashboard on subsribe

--- angular/src/app/main-page/main-page.component.ts
+++ angular/src/app/main-page/main-page.component.ts
@@ -26,7 +26,7 @@ export class MainPageComponent implements OnInit {
   			{
   				console.log(result);
   			});
-  		this.router.navigate(['./main']);
+  		this.router.navigate(['./dashboard']);
 	}
 
   
