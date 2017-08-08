da91e9ffa4cb47eae5b11c05da7a07e7181ad34c
Alin Paunescu <apaunesc@sfu.ca>
2017-08-08 00:49:26 -0700

    Subscribing sends email with info

--- angular/src/app/main-page/main-page.component.ts
+++ angular/src/app/main-page/main-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpParams} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+	id: number;
+  	private sub: any;
+	subscribePost = '';
+	email = '';
+	
+	subscribe() {
+	    this.subscribePost = 'You are subscribed';
+	    console.log(this.email);
+	    this.http
+  		.post('/api/report/', {message:this.email, id:9999999999},)
+  		.subscribe(result => 
+  			{
+  				console.log(result);
+  			});
+  		this.router.navigate(['./main']);
+	}
 
-  constructor() { }
+  
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   }
-
 }
+
